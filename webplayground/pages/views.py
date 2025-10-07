476be7f8ca342ff1e8ba3db85147e9679769f792
victor-sc12 <victorsuquilanda8639@outlook.com>
2025-10-07 14:30:03 -0500

    Implementación de decorador, en lugar de mixin, para verificar staff user.

--- webplayground/pages/views.py
+++ webplayground/pages/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404, get_list_or_404, redirect
 from .models import Page
 from django.urls import reverse, reverse_lazy
+from django.contrib.admin.views.decorators import staff_member_required
+from django.utils.decorators import method_decorator
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView
@@ -11,9 +13,9 @@ from .forms import PageForm
 
 # Mixin Class para verificar user loged:
 class StaffRequiredMixin(object):
+
+    @method_decorator(staff_member_required)
     def dispatch(self, request, *args, **kwargs):
-        if not request.user.is_staff:
-            return redirect(reverse_lazy('admin:login'))
         return super(StaffRequiredMixin, self).dispatch(request, *args, **kwargs)
 
 # Create your views here.
@@ -26,6 +28,7 @@ class PageDetailView(DetailView):
     # def get(self, request, *args, **kwargs):
     #     return render(request, 'pages/page.html', {'page':self.page})
     
+@method_decorator(staff_member_required, name = 'dispatch')
 class PageCreate(StaffRequiredMixin, CreateView):
     model = Page
     form_class = PageForm
@@ -39,6 +42,7 @@ class PageCreate(StaffRequiredMixin, CreateView):
     # para evitar escribir todo el método reciente, usar método 'reverse_lazy'
     success_url = reverse_lazy('pages:pages')
 
+@method_decorator(staff_member_required, name = 'dispatch')
 class PageUpdate(StaffRequiredMixin, UpdateView):
     model = Page
     fields = ["title", "content", "order"]
@@ -46,7 +50,8 @@ class PageUpdate(StaffRequiredMixin, UpdateView):
 
     def get_success_url(self): 
         return reverse_lazy('pages:page', args = (self.object.id, slugify(self.object.title))) + '?ok'
-    
+
+@method_decorator(staff_member_required, name = 'dispatch')
 class PageDelete(StaffRequiredMixin, DeleteView):
     model = Page
     # template_name_suffix = '_confirm_delete'
